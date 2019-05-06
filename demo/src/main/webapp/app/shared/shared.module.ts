56f8c2084e32fbfd78bd2b5180c71c59c7624f23
François Delbrayelle <fdelbrayelle@gmail.com>
2019-05-06 16:57:27 +0200

    Generated with JHipster 6.0.0

--- demo/src/main/webapp/app/shared/shared.module.ts
+++ demo/src/main/webapp/app/shared/shared.module.ts
@@ -1,21 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
-
-import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { DemoSharedLibsModule, DemoSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 
 @NgModule({
-    imports: [DemoSharedLibsModule, DemoSharedCommonModule],
-    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
-    providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
-    entryComponents: [JhiLoginModalComponent],
-    exports: [DemoSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [DemoSharedLibsModule, DemoSharedCommonModule],
+  declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+  entryComponents: [JhiLoginModalComponent],
+  exports: [DemoSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DemoSharedModule {
-    static forRoot() {
-        return {
-            ngModule: DemoSharedModule
-        };
-    }
+  static forRoot() {
+    return {
+      ngModule: DemoSharedModule
+    };
+  }
 }
